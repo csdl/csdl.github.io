49724aebe79a8307b94e46a36805652e59ce373f
Philip Johnson <johnson@hawaii.edu>
2020-04-01 11:47:02 -1000

    checkpoint

--- src/src/NewsCard.js
+++ src/src/NewsCard.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container'
-import Title from './Title'
 
 function NewsCard(props) {
-  const cardStyle = { width: '18rem' }
+  const cardStyle = { border: "1px solid #376551" };
   return (
-    <div>
-      <Card style={cardStyle} >
-        <Card.Img style={{width: "100px"}} variant="top" src={props.Media}/>
-        <Card.Body>
-          <Card.Title>{props.Headline}</Card.Title>
-          <Card.Text>
-            {props.Text}
-          </Card.Text>
-          <Card.Link href={props.URL}>More Info</Card.Link>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">{props.Date}</small>
-        </Card.Footer>
-      </Card>
-    </div>
+    <Card style={cardStyle}>
+      <Card.Img variant="top" src={props.Media}/>
+      <Card.Body>
+        <Card.Title style={{color: "#376551"}}>{props.Headline}</Card.Title>
+        <Card.Text>
+          {props.Text} ({props.Date})
+        </Card.Text>
+        <Card.Link href={props.URL}>More Info</Card.Link>
+      </Card.Body>
+    </Card>
   );
 }
 
--- src/src/NewsCards.js
+++ src/src/NewsCards.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container'
+import CardColumns from 'react-bootstrap/CardColumns'
+import CardDeck from 'react-bootstrap/CardDeck'
 import CardGroup from 'react-bootstrap/CardGroup'
 import Title from './Title'
 import getNewsCardsData from './NewsCardsData';
 import NewsCard from './NewsCard';
 
-function renderNewsCard(data) {
-  return <NewsCard Date={data.Date} Headline={data.Headline} Text={data.Text} URL={data.URL} Media={data.Media}/>
+function renderNewsCard(data, idx) {
+  return <NewsCard key={idx} Date={data.Date} Headline={data.Headline} Text={data.Text} URL={data.URL} Media={data.Media}/>
+}
+
+function NewsCards(props) {
+  const recent = getNewsCardsData().slice(0,3);
+  return (
+    <div style={props.sectionStyle} id="home">
+      <Container>
+        <Title title={"News"}/>
+         <CardColumns>
+          {recent.map((data, idx) => renderNewsCard(data, idx))}
+         </CardColumns>
+      </Container>
+    </div>
+  );
 }
 
 function NewsCards2(props) {
@@ -15,21 +31,21 @@ function NewsCards2(props) {
     <div style={props.sectionStyle} id="home">
       <Container>
         <Title title={"News"}/>
-        <CardGroup>
-          {getNewsCardsData().map(data => renderNewsCard(data))}
-        </CardGroup>
-      </Container>
-    </div>
-  );
-}
-
-function NewsCards(props) {
-  return (
-    <div style={props.sectionStyle} id="home">
-      <Container>
-        <Title title={"News"}/>
-        <CardGroup>
-          <Card>
+        <CardDeck>
+          <Card style={{ width: '28rem' }}>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title Card title Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card style={{ width: '9rem' }}>
             <Card.Img variant="top" src="holder.js/100px160" />
             <Card.Body>
               <Card.Title>Card title</Card.Title>
@@ -42,7 +58,7 @@ function NewsCards(props) {
               <small className="text-muted">Last updated 3 mins ago</small>
             </Card.Footer>
           </Card>
-          <Card>
+          <Card style={{ width: '9rem' }}>
             <Card.Img variant="top" src="holder.js/100px160" />
             <Card.Body>
               <Card.Title>Card title</Card.Title>
@@ -55,20 +71,7 @@ function NewsCards(props) {
               <small className="text-muted">Last updated 3 mins ago</small>
             </Card.Footer>
           </Card>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
+          <Card style={{ width: '9rem' }}>
             <Card.Img variant="top" src="holder.js/100px160" />
             <Card.Body>
               <Card.Title>Card title</Card.Title>
@@ -81,7 +84,7 @@ function NewsCards(props) {
               <small className="text-muted">Last updated 3 mins ago</small>
             </Card.Footer>
           </Card>
-          <Card>
+          <Card style={{ width: '9rem' }}>
             <Card.Img variant="top" src="holder.js/100px160" />
             <Card.Body>
               <Card.Title>Card title</Card.Title>
@@ -95,7 +98,7 @@ function NewsCards(props) {
               <small className="text-muted">Last updated 3 mins ago</small>
             </Card.Footer>
           </Card>
-        </CardGroup>
+        </CardDeck>
       </Container>
     </div>
   );
