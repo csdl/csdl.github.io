9ba30cc771b87e19adf428e9d13bacaa5ccca7d5
Philip Johnson <johnson@hawaii.edu>
2020-04-16 10:47:33 -1000

    Home now summarizes publications.

--- src/src/Home.js
+++ src/src/Home.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
+import Markdown from 'react-markdown';
+import TechReports from './TechReports';
 import Title from './Title';
+
+const techReports = new TechReports();
+const journal = techReports.getKeysByKeyword('Pubs-Journal').length;
+const conference = techReports.getKeysByKeyword('Pubs-Conference').length;
+const workshop = techReports.getKeysByKeyword('Pubs-Workshop').length;
+const phd = techReports.getKeysByKeyword('Thesis-PhD').length;
+const ms = techReports.getKeysByKeyword('Thesis-MS').length;
+const bs = techReports.getKeysByKeyword('Thesis-BS').length;
+
+const homePageText = `
+The Collaborative Software Development Laboratory is led by [Philip Johnson](https://philipmjohnson.org"), a Professor in the [Department of Information and Computer Sciences](http://www.ics.hawaii.edu) at the [University of Hawaii](https://www.hawaii.edu).
+
+Established in 1991, the Collaborative Software Development Laboratory performs research, development, and technology transfer in disciplines including software engineering, sustainability, entrepreneurship, educational technology, renewable energy, and computer-supported cooperative work.
+
+So far, CSDL members have performed research resulting in ${journal} journal, ${conference} conference, and ${workshop} workshop publications, as well as ${phd} Ph.D. theses, ${ms} M.S. theses, and ${bs} undergraduate honors theses.  
+`;
 
 function Home(props) {
   const jumbotronStyle = { backgroundColor: '#FFFFFF', padding: 0, marginBottom: 0 };
@@ -11,16 +29,7 @@ function Home(props) {
       <Jumbotron fluid style={jumbotronStyle}>
         <Container>
           <Title title={'Welcome to CSDL'}/>
-          <p>
-            The Collaborative Software Development Laboratory is led by <a href="https://philipmjohnson.org">Philip
-            Johnson</a>, a Professor in the <a href="http://www.ics.hawaii.edu">Department of Information and Computer
-            Sciences</a> at the <a href="https://www.hawaii.edu">University of Hawaii</a>.
-          </p>
-          <p>
-            Established in 1991, the Collaborative Software Development Laboratory performs research, development, and
-            technology transfer in disciplines including software engineering, sustainability, entrepreneurship,
-            educational technology, renewable energy, and computer-supported cooperative work.
-          </p>
+          <Markdown source={homePageText}/>
         </Container>
       </Jumbotron>
     </div>
