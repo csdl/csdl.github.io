ecd25227982c469f919ffeb190870225401d6f53
Philip Johnson <johnson@hawaii.edu>
2020-04-18 13:19:57 -1000

    All photos in the frame.

--- src/src/data/PeopleData.js
+++ src/src/data/PeopleData.js
@@ -28,7 +28,7 @@ export default function getPeopleData() {
     {
       name: ['Amanda', 'Pacholok'],
       photo: '/images/people/amanda-pacholok.jpg',
-      dates: ['2012', '2013']
+      dates: ['2012', '2013'],
     },
     {
       name: ['Amy', 'Takayesu'],
@@ -36,14 +36,89 @@ export default function getPeopleData() {
       dates: ['2016', '2017'],
     },
     {
+      name: ['Andrea', 'Connell'],
+      photo: '/images/people/andrea-connell.jpg',
+      dates: ['2012', ''],
+    },
+    {
+      name: ['Anne', 'Disney'],
+      photo: '/images/people/anne-disney.jpg',
+      dates: ['1996', '1998'],
+    },
+    {
       name: ['Anthony', 'Christe'],
       photo: '/images/people/anthony-christe.jpg',
       dates: ['2013', 'present'],
     },
     {
-      name: ['Carleton', 'Moore'],
+      name: ['Austin', 'Ito'],
+      photo: '/images/people/austen-ito.jpg',
+      dates: ['2004', '2005'],
+    },
+    {
+      name: ['Cam', 'Moore'],
       photo: '/images/people/cam-moore.jpg',
       dates: ['1993', 'present'],
+    },
+    {
+      name: ['Caterina', 'Desiato'],
+      photo: '/images/people/caterina-desiato.jpg',
+      dates: ['2011', ''],
+    },
+    {
+      name: ['Cedric', 'Zhang'],
+      photo: '/images/people/cedric-zhang.jpg',
+      dates: ['2003', '2007'],
+    },
+    {
+      name: ['Christoph', 'Lofi'],
+      photo: '/images/people/christoph-lofi.jpg',
+      dates: ['2005', ''],
+    },
+    {
+      name: ['Chris', 'Zorn'],
+      photo: '/images/people/chris-zorn.jpg',
+      dates: ['2011', '2014'],
+    },
+    {
+      name: ['Dadong', 'Wan'],
+      photo: '/images/people/dadong-wan.png',
+      dates: ['1991', '1994'],
+    },
+    {
+      name: ['Danny', 'Weng'],
+      photo: '/images/people/danny-weng.jpg',
+      dates: ['2017', '2018'],
+    },
+    {
+      name: ['Danu', 'Tjahjono'],
+      photo: '/images/people/danu-tjahjono.jpg',
+      dates: ['1991', '1996'],
+    },
+    {
+      name: ['David', 'Aghalarpour'],
+      photo: '/images/people/david-aghalarpour.jpg',
+      dates: ['2015', '2018'],
+    },
+    {
+      name: ['David', 'Badke'],
+      photo: '/images/people/david-badke.jpg',
+      dates: ['2017', ''],
+    },
+    {
+      name: ['David', 'Nickles'],
+      photo: '/images/people/david-nickles.jpg',
+      dates: ['2007', '2008'],
+    },
+    {
+      name: ['Evan', 'Hataishi'],
+      photo: '/images/people/evan-hataishi.jpg',
+      dates: ['2017', '2018'],
+    },
+    {
+      name: ['George', 'Lee'],
+      photo: '/images/people/george-lee.jpg',
+      dates: ['2010', '2012'],
     },
     {
       name: ['Gian', 'Calica'],
@@ -51,9 +126,149 @@ export default function getPeopleData() {
       dates: ['2019', 'present'],
     },
     {
+      name: ['Glen', 'Barcelo'],
+      photo: '/images/people/glen-barcelo.jpg',
+      dates: ['2019', ''],
+    },
+    {
+      name: ['Greg', 'Burgess'],
+      photo: '/images/people/greg-burgess.jpg',
+      dates: ['2011', '2013'],
+    },
+    {
+      name: ['Hana', 'Bowers'],
+      photo: '/images/people/hana-bowers.jpg',
+      dates: ['2011', ''],
+    },
+    {
+      name: ['Hongbing', 'Kou'],
+      photo: '/images/people/hongbing-kou.jpg',
+      dates: ['2002', '2007'],
+    },
+    {
+      name: ['James', 'Wang'],
+      photo: '/images/people/james-wang.jpg',
+      dates: ['', ''],
+    },
+    {
+      name: ['Jay', 'Corbett'],
+      photo: '/images/people/jay-corbett.jpg',
+      dates: ['1997', '2000'],
+    },
+    {
+      name: ['Jennifer', 'Geis'],
+      photo: '/images/people/jennifer-geis.jpg',
+      dates: ['1996', '1998'],
+    },
+    {
+      name: ['Jitender', 'Miglani'],
+      photo: '/images/people/jitender-miglani.jpg',
+      dates: ['2000', '2002'],
+    },
+    {
+      name: ['Joe', 'Dane'],
+      photo: '/images/people/joe-dane.jpg',
+      dates: ['1998', '1999'],
+    },
+    {
+      name: ['Jordan', 'Takayama'],
+      photo: '/images/people/jordan-takayama.png',
+      dates: ['2013', '2014'],
+    },
+    {
+      name: ['Josephine', 'Garces'],
+      photo: '/images/people/josephine-garces.jpg',
+      dates: ['2016', '2017'],
+    },
+    {
+      name: ['Joy', 'Agustin'],
+      photo: '/images/people/joy-agustin.jpg',
+      dates: ['2001', '2003'],
+    },
+    {
+      name: ['Julie', 'Sakuda'],
+      photo: '/images/people/julie-sakuda.png',
+      dates: ['2004', '2005'],
+    },
+    {
+      name: ['Julio', 'Polo'],
+      photo: '/images/people/julio-polo.jpg',
+      dates: ['1993', '1994'],
+    },
+    {
+      name: ['Ka Yee', 'Leung'],
+      photo: '/images/people/ka-yee-leung.jpg',
+      dates: ['2007', '2009'],
+    },
+    {
+      name: ['Kaila', 'Foltz'],
+      photo: '/images/people/kaila-foltz.jpg',
+      dates: ['2018', ''],
+    },
+    {
+      name: ['Kea', 'Uehara'],
+      photo: '/images/people/kea-uehara.jpg',
+      dates: ['2016', '2017'],
+    },
+    {
       name: ['Mercedez', 'Castro'],
       photo: '/images/people/mercedez-castro.jpg',
       dates: ['2019', 'present'],
+    },
+    {
+      name: ['Mette', 'Moffett'],
+      photo: '/images/people/mette-moffett.jpg',
+      dates: ['1999', '2001'],
+    },
+    {
+      name: ['Michael', 'Paulding'],
+      photo: '/images/people/michael-paulding.png',
+      dates: ['2003', '2006'],
+    },
+    {
+      name: ['Michael', 'Staver'],
+      photo: '/images/people/michael-staver.jpg',
+      dates: ['2000', '2002'],
+    },
+    {
+      name: ['Michelle', 'Katchuck'],
+      photo: '/images/people/michelle-katchuck.jpg',
+      dates: ['2011', '2014'],
+    },
+    {
+      name: ['Mirabela', 'Medallon'],
+      photo: '/images/people/mirabela-medallon.jpg',
+      dates: ['2019', 'present'],
+    },
+    {
+      name: ['Monir', 'Hodges'],
+      photo: '/images/people/monir-hodges.jpg',
+      dates: ['1999', '2000'],
+    },
+    {
+      name: ['Morgan', 'de Partee'],
+      photo: '/images/people/morgan-departee.jpg',
+      dates: ['2012', '2013'],
+    },
+    {
+      name: ['Nathaniel', 'Ashe'],
+      photo: '/images/people/nathaniel-ashe.jpg',
+      dates: ['2010', '2011'],
+    },
+    {
+      name: ['Pavel', 'Senin'],
+      photo: '/images/people/pavel-senin.jpg',
+      dates: ['2006', '2015'],
+    },
+    {
+      name: ['Peter', 'Leong'],
+      photo: '/images/people/peter-leong.jpg',
+      dates: ['2019', 'present'],
+    },
+    {
+      name: ['Philip', 'Johnson'],
+      photo: '/images/people/philip-johnson.jpg',
+      dates: ['1991', 'present'],
     },
     {
       name: ['Quinne', 'Uchida'],
@@ -61,20 +276,79 @@ export default function getPeopleData() {
       dates: ['2019', 'present'],
     },
     {
-      name: ['Serge', 'Negrashov'],
+      name: ['Randy', 'Cox'],
+      photo: '/images/people/randy-cox.jpg',
+      dates: ['2008', '2009'],
+    },
+    {
+      name: ['Risa', 'Khamsi'],
+      photo: '/images/people/risa-khamsi.jpg',
+      dates: ['2011', '2012'],
+    },
+    {
+      name: ['Robert', 'Brewer'],
+      photo: '/images/people/robert-brewer.jpg',
+      dates: ['1993', '2013'],
+    },
+    {
+      name: ['Rosemary', 'Sumajit'],
+      photo: '/images/people/rosemary-sumajit.jpg',
+      dates: ['1994', '1995'],
+    },
+    {
+      name: ['Russell', 'Tokuyama'],
+      photo: '/images/people/russell-tokuyama.jpg',
+      dates: ['1996', '1998'],
+    },
+    {
+      name: ['Sergey', 'Negrashov'],
       photo: '/images/people/sergey-negrashov.jpg',
       dates: ['2013', 'present'],
     },
     {
-      name: ['Mirabela', 'Medallon'],
-      photo: '/images/people/mirabela-medallon.jpg',
+      name: ['Seungoh', 'Paek'],
+      photo: '/images/people/seungoh-paek.jpg',
       dates: ['2019', 'present'],
     },
-
     {
-      name: ['Philip', 'Johnson'],
-      photo: '/images/people/philip-johnson.jpg',
-      dates: ['1991', 'present'],
+      name: ['Shanah', 'Trevenna'],
+      photo: '/images/people/shanah-trevenna.jpg',
+      dates: ['2010', ''],
+    },
+    {
+      name: ['Shaoxuan', 'Zhang'],
+      photo: '/images/people/shaoxuan-zhang.jpg',
+      dates: ['', ''],
+    },
+    {
+      name: ['Takuya', 'Yamashita'],
+      photo: '/images/people/takuya-yamashita.jpg',
+      dates: ['2003', '2005'],
+    },
+    {
+      name: ['Tina', 'Sablan'],
+      photo: '/images/people/tina-sablan.jpg',
+      dates: ['2013', '2014'],
+    },
+    {
+      name: ['Tryggvi', 'Bjorgvinsson'],
+      photo: '/images/people/tryggvi-bjorgvinsson.jpg',
+      dates: ['2008', '2009'],
+    },
+    {
+      name: ['Tyler', 'Mongan'],
+      photo: '/images/people/tyler-mongan.jpg',
+      dates: ['2013', '2015'],
+    },
+    {
+      name: ['William', 'Albritton'],
+      photo: '/images/people/william-albritton.jpg',
+      dates: ['2001', '2002'],
+    },
+    {
+      name: ['Yongwen', 'Xu'],
+      photo: '/images/people/yongwen-xu.jpg',
+      dates: ['2011', '2015'],
     },
   ];
 }
