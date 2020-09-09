ca513a78e03ea7c1ce8eddfc755e3384e469434f
Philip Johnson <johnson@hawaii.edu>
2020-09-09 09:09:35 -1000

    Updates

---
+++ precache-manifest.6513db0562d1e4d025aed239d8aed5b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59394b9059cd486f08653471f165b572",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dfb65fa1ca8f67d9a7c",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "76fc6b5e0e6bd990eba1",
+    "url": "/static/css/main.467e710e.chunk.css"
+  },
+  {
+    "revision": "8dfb65fa1ca8f67d9a7c",
+    "url": "/static/js/2.74a6ec4a.chunk.js"
+  },
+  {
+    "revision": "456711914460e659dfbbbb8bf79f5cfc",
+    "url": "/static/js/2.74a6ec4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76fc6b5e0e6bd990eba1",
+    "url": "/static/js/main.54efa147.chunk.js"
+  },
+  {
+    "revision": "dcad35ce53814db73077",
+    "url": "/static/js/runtime-main.a7dc1534.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e4b58f00da3b7805bf36df6835e0081f.js"
+  "/precache-manifest.6513db0562d1e4d025aed239d8aed5b6.js"
 );
 
 self.addEventListener('message', (event) => {
